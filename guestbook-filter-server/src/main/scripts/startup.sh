42c67f75d662c4daf0944883cfdeccac02ddd168
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2017-04-16 13:43:54 +0200

    chore(confd): enable confd for the filter

---
+++ guestbook-filter-server/src/main/scripts/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/ash
+
+if [ -e /data/env.sh ]
+then
+  echo "Sourcing /data/env.sh"
+  source /data/env.sh
+else
+  echo "/!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\"
+  echo "No /data/env.sh found, you've been warned!"
+  echo "/!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\"
+fi
+
+if /usr/local/bin/confd -onetime -backend env
+  then
+    echo
+    echo "w00t w00t!! config has been generated, let's start our sprinbgoot app now"
+    java -jar /usr/local/appli/springboot-app.jar
+ else
+    echo "You failed at starting confd and/or springboot properly"
+fi
