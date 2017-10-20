13be57fa5bd95c7059d6a449c0a556436f695a12
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2017-10-20 18:26:53 +0200

    chore(startup): make script ok for shellcheck

--- guestbook-simple-frontend/src/main/scripts/startup.sh
+++ guestbook-simple-frontend/src/main/scripts/startup.sh
@@ -2,18 +2,19 @@
 
 DICT_LOCATION=/config/dictionaries/env.sh
 if [ -e "${DICT_LOCATION}" ]; then
-	echo "Sourcing "${DICT_LOCATION}""
-	source "${DICT_LOCATION}"
+    echo "Sourcing ${DICT_LOCATION}"
+    # shellcheck source=/dev/null
+    source "${DICT_LOCATION}"
 else
-	echo "/!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\"
-	echo "No "${DICT_LOCATION}" found, you've been warned!"
-	echo "/!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\"
+    echo "/!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\"
+    echo "No '${DICT_LOCATION}' found, you've been warned!"
+    echo "/!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\"
 fi
 
 if /usr/local/bin/confd -onetime -backend env; then
-	echo
-	echo "w00t w00t!! config has been generated, let's start nginx now"
-	nginx -g 'daemon off;'
+    echo
+    echo "w00t w00t!! config has been generated, let's start nginx now"
+    nginx -g 'daemon off;'
 else
-	echo "You failed at starting confd and/or nginx properly"
+    echo "You failed at starting confd and/or nginx properly"
 fi
